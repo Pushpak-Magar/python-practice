d38352c9553d18cb278984021911302a9e5d4614
Pushpak-Magar <pushpakmagar51@gmail.com>
2025-07-01 23:30:03 +0530

    square-cube file

---
+++ square-cube/main.py
@@ -0,0 +1,10 @@
+def square_cube():
+    num=int(input("enter a number:"))
+
+    square= num **2
+    cube=num **3
+
+    print(f"square of {num} is {square}")
+    print(f"cube of{num} is {cube}")
+
+square_cube()
