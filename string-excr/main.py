0c5a5a767ecad0c171e244b5cfa67424dcfe3b84
Pushpak-Magar <pushpakmagar51@gmail.com>
2025-07-01 13:29:41 +0530

    string exercise file

---
+++ string-excr/main.py
@@ -0,0 +1,17 @@
+numbers = []
+strings = []
+names = ["Steve", "Bruce", "harry"]
+
+numbers.append(9)
+numbers.append(5)
+numbers.append(3)
+
+strings.append("Avengers")
+strings.append("assemble")
+
+second_name = names[1]
+
+# this code should write out the filled arrays and the second name in the names list
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
