9cd6219c519a1a20d077dda622e93e7d34be21bd
Pushpak-Magar <pushpakmagar51@gmail.com>
2025-07-01 14:05:58 +0530

    arithmetic operarions file1

---
+++ arth-operators/main.py
@@ -0,0 +1,13 @@
+def operations():
+    num1=5
+    num2=8
+
+    add=num2+num1
+    sub=num2-num1
+    mul=num2*num1
+    div=num2/num1
+
+    print("arithmetuc operations on two numbers")
+    print(add,sub,mul,div)
+
+operations()
