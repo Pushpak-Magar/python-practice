18d92203a26ba94f2a9b7746875f962b135181fc
Pushpak-Magar <pushpakmagar51@gmail.com>
2025-07-01 23:36:25 +0530

    another variation of square-cube

---
+++ nikhil_example/main.py
@@ -0,0 +1,12 @@
+import math
+def nikhil_bhoii():
+    cube=int(input("enter the cube value:"))
+    num=math.cbrt(cube)
+    print(f"cube root of {cube} is {num}")
+
+
+    square=num**2
+    print(f"square of {num} is {square}")
+
+nikhil_bhoii()
+
