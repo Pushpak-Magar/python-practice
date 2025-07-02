fec8346f6e462bd67204cbce6033d82002be798b
Pushpak-Magar <pushpakmagar51@gmail.com>
2025-07-02 10:43:34 +0530

    multiple arguments along with string and their format speciefiers

---
+++ string-formatting-2/main.py
@@ -0,0 +1,9 @@
+def stringformatting():
+    fullname="john" + " " + "wick"
+    age=40
+    address="new" + " " + "york"
+    print("%s is a %d years old man , who lives in %s" % (fullname,age,address))
+
+stringformatting()
+
+ 
