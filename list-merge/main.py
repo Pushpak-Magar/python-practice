47c40607a47c9b547c30af6936d19e8a7c5093ca
Pushpak-Magar <pushpakmagar51@gmail.com>
2025-07-02 10:18:59 +0530

    list additions and list merge done

---
+++ list-merge/main.py
@@ -0,0 +1,21 @@
+def even_odd_addition():
+    even_num=[2,4,6,8]
+    odd_num=[1,3,5,7]
+
+    for i in range(len(even_num)):
+        addition= even_num[i] + odd_num[i]
+        print(addition)
+
+
+
+even_odd_addition()
+
+
+def listmerge():
+    list1=[2,4,6,8]
+    list2=[1,3,5,7]
+
+    addition=list2 + list1
+    print(addition)
+
+listmerge()
