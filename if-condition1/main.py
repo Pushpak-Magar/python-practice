40b4d5c1ead0bfbad8da42d60aae79b4c4b61aa0
Pushpak-Magar <pushpakmagar51@gmail.com>
2025-07-08 16:10:11 +0530

    if condition file 1

---
+++ if-condition1/main.py
@@ -0,0 +1,17 @@
+"""The variables a and b have missing values, fill them so that the code inside 
+ the if statement will be executed! (make sure the if condition is true)
+
+# At the end of the program, the value of c should be 3."""
+
+a=15
+b=11
+
+c=0
+
+if a >= b and not b < 10: #condition
+    c=2
+
+c+= 1
+print(f"c= {c}")
+
+
