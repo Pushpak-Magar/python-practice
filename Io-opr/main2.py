4f0c08d219535a67e57d81b27fa9558f4f37a3cc
Pushpak-Magar <pushpakmagar51@gmail.com>
2025-07-11 23:46:46 +0530

    IO one more file added

---
+++ Io-opr/main2.py
@@ -0,0 +1,9 @@
+# Write a program that gets the user's age as input.
+
+# The program will output (print) the number of missing years till 120 (in a specific format, shown below).
+
+# For example, for input 25, the expected output is "95 years till 120".
+
+age = int(input())
+
+print(f"{120 - age } years till 120 .")
