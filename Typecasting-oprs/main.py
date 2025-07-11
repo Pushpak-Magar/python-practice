19408fd953ac9903ba21592e6d2c3fe272acb18e
Pushpak-Magar <pushpakmagar51@gmail.com>
2025-07-11 23:41:20 +0530

    Typecasting oprs in IO done

---
+++ Typecasting-oprs/main.py
@@ -0,0 +1,27 @@
+# To receive multiple inputs from the user write them multiple times:
+
+# var1 = input()
+# var2 = input()
+# Every test case contains two inputs.
+
+# Store the inputs in two variables, cast them to float and print the multiplication of the two.
+
+var1 = float(input())
+var2 = float(input())
+
+print(f"{var1 * var2}")
+
+
+# Write a program that takes two inputs from the user. These inputs should be treated as numbers with decimal points.
+
+""" Store the inputs in two variables, convert them to float type, 
+ and print the result of dividing the first number by the second number."""
+
+num1 = float(input())
+num2 = float(input())
+
+print(f"{num1 / num2}")
+
+
+
+
